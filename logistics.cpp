0015385c718a341ddd4188c750b218b024a971cf
Axel Ramirez Linares <ramirez.a19@outlook.com>
2020-03-24 21:18:28 -0600

    Clases

--- logistics.cpp
+++ logistics.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
 
+class drone
+{
+public:
+    int ID;//Número de identificación del drone
+    int state;
+    /*Estado del drone
+    0->Disponible
+    1->Transporte
+    2->Inspección*/
+    int battery //Porcentaje de batería del drone
+};
+
+class package
+{
+public:
+    int weight;//Peso del paquete
+    char address;// Direccion del paquete: A,B,C,D
+    bool priority;
+    /*Tipo de envío
+    0 -> envio normal
+    1 -> envío express
+    */
+};
+
+class conveyor_belt
+{
+public:
+    int length;//longitud de la banda
+    int speed;//velocidad de movmiento de la banda
+
+};
 
 
 int main(void){
